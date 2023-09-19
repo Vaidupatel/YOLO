9dd4f9ae126dab16c2963b48023c21800aaf7060
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-09-19 20:32:08 +0530

    This is first model with classes crop and weed

--- Training.py
+++ Training.py
@@ -2,12 +2,12 @@ from ultralytics import YOLO
 
 
 # Load a model
-# model = YOLO("yolov8n.yaml") # If we want to create the new model from scratch then use this
+model = YOLO("yolov8n.yaml") # If we want to create the new model from scratch then use this
 
 save_dir = r"C:\Users\admin\PycharmProjects\YOLO"
 
 # load a pretrained model (If we are training our existing model than use this)
-model = YOLO(r'C:\Users\admin\PycharmProjects\YOLO\runs\detect\train\weights\best.pt')
+# model = YOLO(r'C:\Users\admin\PycharmProjects\YOLO\runs\detect\train\weights\best.pt')
 
 # Use the mode(Give the path of config.yaml file in which we have classified the classes)
 model.train(data=r"C:\Users\admin\PycharmProjects\YOLO\config.yaml", project=save_dir, epochs=1)  # train the model
