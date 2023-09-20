cb5787b2082c14b034d8ad47ff1192fa431c016a
nirjalahir <ambaliyanirjal2@gmail.com>
2023-09-20 12:39:53 +0530

    the first successful model

--- Training.py
+++ Training.py
@@ -1,13 +1,10 @@
 from ultralytics import YOLO
 
-
 # Load a model
-model = YOLO("yolov8n.yaml") # If we want to create the new model from scratch then use this
-
-save_dir = r"C:\Users\admin\PycharmProjects\YOLO"
+# model = YOLO("yolov8n.yaml") # If we want to create the new model from scratch then use this
 
 # load a pretrained model (If we are training our existing model than use this)
-# model = YOLO(r'C:\Users\admin\PycharmProjects\YOLO\runs\detect\train\weights\best.pt')
+model = YOLO(r'yolov8n.pt')
 
 # Use the mode(Give the path of config.yaml file in which we have classified the classes)
-model.train(data=r"C:\Users\admin\PycharmProjects\YOLO\config.yaml", project=save_dir, epochs=1)  # train the model
+model.train(data=r"C:\Users\HP\PycharmProjects\YOLO\config.yaml", epochs=1)  # train the model
