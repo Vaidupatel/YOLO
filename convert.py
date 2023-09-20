63c6d059681c7a61f9c03dfb0f55f014d818300f
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-09-20 20:59:18 +0530

    This is first model with class weed

---
+++ convert.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import json
+
+# Folder containing JSON files
+json_folder = Path(r'C:\Users\admin\PycharmProjects\YOLO\xtra\Annonate\Goosegrass\label')
+
+# Loop through JSON files
+for json_file in json_folder.glob('*.json'):
+
+  # Load JSON data
+  with open(json_file) as f:
+    data = json.load(f)
+
+  label = data['shapes'][0]['label']
+  points = data['shapes'][0]['points']
+
+  if label == "weeds":
+    label = "1"
+  else:
+    label = "0"
+
+  point_str = ' '.join([str(p) for coords in points for p in coords])
+
+  # Construct output text file path
+  txt_file = json_file.with_suffix('.txt')
+
+  # Write label and points to text file
+  with open(txt_file, 'w') as f:
+    f.write(label + ' ' + point_str)
