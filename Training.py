52448fe7ef4592b9a2732f707b3672d888d2e013
Admin <vaidpatel11@gmail.com>
2023-09-21 11:24:47 +0530

    Upload the data of carpet weed

--- Training.py
+++ Training.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
 # Load a model
-# model = YOLO("yolov8n.yaml") # If we want to create the new model from scratch then use this
+model = YOLO(r"C:\Users\Admin\PycharmProjects\YOLO\train\weights\best.pt") # If we want to create the new model from scratch then use this
 
 # load a pretrained model (If we are training our existing model than use this)
-model = YOLO(r'yolov8n.pt')
+# model = YOLO(r'yolov8n.pt')
 
-save_dir = r"C:\Users\admin\PycharmProjects\YOLO"
+save_dir = r"C:\Users\Admin\PycharmProjects\YOLO"
 # Use the mode(Give the path of config.yaml file in which we have classified the classes)
-model.train(data=r"C:\Users\admin\PycharmProjects\YOLO\config.yaml", epochs=5, project=save_dir)  # train the model
+model.train(data=r"C:\Users\admin\PycharmProjects\YOLO\config.yaml", epochs=1, project=save_dir)  # train the model
--- convert.py
+++ convert.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import json
 
 # Folder containing JSON files
-json_folder = Path(r'C:\Users\admin\PycharmProjects\YOLO\xtra\Annonate\Goosegrass\label')
+json_folder = Path(r'D:\Project\Carpetweeds\Label')
 
 # Loop through JSON files
 for json_file in json_folder.glob('*.json'):
